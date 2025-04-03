447187cbbcfebf2003b8a5ec56faa8f42a5e3ebc
Николай Варюшин <pugsassts@gmail.com>
2025-04-03 14:54:22 +0300

    add favicons and normolize

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const FaviconsWabpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
@@ -13,10 +14,10 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
+      // {
+      //   test: /\.(woff2?|eot|ttf|otf)$/i,
+      //   type: 'asset/resource',
+      // },
       {
         test: /\.js$/,
         use: 'babel-loader',
@@ -61,7 +62,34 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css'
-    })
+    }),
+    new FaviconsWabpackPlugin({
+      logo: 'src/favicons/favicon.svg',
+      mode: 'webapp',
+      devMode: 'webapp',
+      prefix: 'favicons/',
+      cache: true,
+      inject: true,
+      favicons: {
+        icons: {
+          android: [
+            "android-chrome-144x144.png",
+            "android-chrome-192x192.png",
+            "android-chrome-384x384.png"
+          ],
+          appleIcon: ["apple-touch-icon-180x180.png"],
+          appleStartup: false,
+          favicons: [
+            "favicon.svg",
+            "favicon.ico"
+          ],
+          firefox: false,
+          windows: false,
+          coast: false,
+          yandex: false,
+        },
+      },
+    }),
   ],
   optimization: {
     minimizer: [
