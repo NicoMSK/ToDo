e078a43c6afd5c17a0845f5856ea533c17c98090
Николай Варюшин <pugsassts@gmail.com>
2025-05-01 17:19:19 +0300

    refactor: redesign the task editing block for the MVС architecture

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import 'normalize.css';
 import './main.scss';
 import './scripts/form/controllerForm.js';
 import './scripts/dialog/controllerDialog.js';
-import './scripts/editTask.js';
+import './scripts/edit/controllerEditTask.js';
 import './scripts/deleteTask.js';
--- src/scripts/edit/modelEditTask.js
+++ src/scripts/edit/modelEditTask.js
@@ -1,5 +1,3 @@
-const todoList = document.querySelector(".hero__list");
-
 function getClickedButton(event) {
   return event.target.closest('[data-type]');
 };
@@ -35,7 +33,7 @@ function cancelEditMode(event) {
   todoItem.classList.remove('hero__item--edit');
 }
 
-function checkIfElementHasClass(event) {
+export function checkIfElementHasClass(event) {
   const clickedButton = getClickedButton(event);
   if (!clickedButton) return;
 
@@ -60,8 +58,4 @@ function checkIfElementHasClass(event) {
     default:
       throw new Error('Ошибка, кнопка не найдена')
   }
-}
-
-todoList.addEventListener('click', checkIfElementHasClass);
-
-
+};
